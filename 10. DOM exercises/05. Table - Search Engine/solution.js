7bc5c74d8a38c7b2aea3de8dfc0454a8db938b56
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-06-08 23:10:10 +0300

    Solved Problem 5.

--- 10. DOM exercises/05. Table - Search Engine/solution.js
+++ 10. DOM exercises/05. Table - Search Engine/solution.js
@@ -1,8 +1,18 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
+   let rows = document.querySelectorAll('tbody tr');
+   let input = document.getElementById('searchField');
    function onClick() {
-      //   TODO:
 
+      for (const row of rows) {
+         row.className = '';
+      }
+
+      for (let row of rows) {
+         if (row.innerHTML.includes(input.value)) {
+            row.className = 'select';
+         }
+      }
    }
 }
