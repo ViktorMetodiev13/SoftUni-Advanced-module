bd40d4f2a231ced0be1622aada0005951ec2a531
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-06-08 20:53:26 +0300

    Solved Problem 3.

--- 10. DOM exercises/03. Accordion/accordion.js
+++ 10. DOM exercises/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let textElement = document.getElementById('extra');
+    let button = document.getElementsByClassName('button')[0];
+
+    if (button.textContent == 'More') {
+        button.textContent = 'Less';
+        textElement.style.display = 'block';
+    } else {
+        button.textContent = 'More';
+        textElement.style.display = 'none';
+    }
 }
