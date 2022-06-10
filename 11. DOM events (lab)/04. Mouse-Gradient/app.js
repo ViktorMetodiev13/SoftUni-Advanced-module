fa99088e6a609ae51df5792f3d9ba14940108306
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-06-10 23:23:17 +0300

    Solved Problem 4.

--- 11. DOM events (lab)/04. Mouse-Gradient/app.js
+++ 11. DOM events (lab)/04. Mouse-Gradient/app.js
@@ -1,3 +1,9 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradient = document.getElementById('gradient');
+    gradient.addEventListener('mousemove', onMouseOver);
+    const result = document.getElementById('result');
+
+    function onMouseOver(event) {
+        result.textContent = Math.floor(event.offsetX / gradient.clientWidth * 100) + '%';
+    }
 }
