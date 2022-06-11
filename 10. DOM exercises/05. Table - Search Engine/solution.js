51ae49e8e71d33c7d5228c541cddfb773f7f59a4
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-06-11 17:24:02 +0300

    Changes on Problem 5.

--- 10. DOM exercises/05. Table - Search Engine/solution.js
+++ 10. DOM exercises/05. Table - Search Engine/solution.js
@@ -4,12 +4,8 @@ function solve() {
    let rows = document.querySelectorAll('tbody tr');
    let input = document.getElementById('searchField');
    function onClick() {
-
-      for (const row of rows) {
-         row.className = '';
-      }
-
       for (let row of rows) {
+         row.className = '';
          if (row.innerHTML.includes(input.value)) {
             row.className = 'select';
          }
