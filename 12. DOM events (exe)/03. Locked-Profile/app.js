f53a1f9b59f15f9734f971b9599147c0156bc9f0
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-06-12 12:55:50 +0300

    Solved Problem 4.

--- 12. DOM events (exe)/03. Locked-Profile/app.js
+++ 12. DOM events (exe)/03. Locked-Profile/app.js
@@ -1,20 +1,20 @@
 function lockedProfile() {
     Array.from(document.querySelectorAll('.profile button'))
-    .forEach(b => b.addEventListener('click', toggle));
+        .forEach(b => b.addEventListener('click', toggle));
 
     function toggle(event) {
-        const profile = event.target.parentElement;
-        const isActive = document.querySelector('input[type="radio"][value="unlock"]').checked;
+        let profile = event.target.parentElement;
+        let isActive = document.querySelector('input[type="radio"][value="unlock"]').checked;
 
         if (isActive) {
             let div = profile.querySelector('div');
 
-            if (event.target.textContent == 'Show more') {
-                div.style.display = 'block';
+            if (event.target.textContent == "Show more") {
                 event.target.textContent = 'Hide it';
+                div.style.display = 'block';
             } else {
-                div.style.display = '';
                 event.target.textContent = 'Show more';
+                div.style.display = '';
             }
         }
     }
