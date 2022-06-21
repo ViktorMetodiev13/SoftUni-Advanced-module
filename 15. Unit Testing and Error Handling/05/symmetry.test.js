1062ae4da8669b49974a5e17ec474dfaaa79a6e4
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-06-21 21:17:23 +0300

    Solved Problem 6.

---
+++ 15. Unit Testing and Error Handling/05/symmetry.test.js
@@ -0,0 +1,8 @@
+const { expect } =  require('chai');
+const { isSymmetric } = require('./05. symmetry');
+
+describe('Symmetry Checker', () => {
+    it('works', () => {
+        'do nothing';
+    });
+});
