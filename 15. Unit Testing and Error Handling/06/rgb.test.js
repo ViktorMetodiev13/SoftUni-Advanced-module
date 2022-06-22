59e338bdef097d6f349a600238da43fa493d362c
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-06-22 16:17:33 +0300

    Changes

---
+++ 15. Unit Testing and Error Handling/06/rgb.test.js
@@ -0,0 +1,46 @@
+const { expect } = require('chai');
+const { rgbToHexColor } = require('./rgb');
+
+describe('Testing RGBtoHexColor', () => {
+    it('converts black', () => {
+        expect(rgbToHexColor(0, 0, 0)).to.equal('#000000')
+    });
+
+    it('converts white', () => {
+        expect(rgbToHexColor(255, 255, 255)).to.equal('#FFFFFF')
+    });
+
+    it('converts 15,15,15 to #0F0F0F', () => {
+        expect(rgbToHexColor(15, 15, 15)).to.equal('#0F0F0F')
+    });
+
+    it('returns undefined for missing params', () => {
+        expect(rgbToHexColor(0, 0)).to.be.undefined;
+        expect(rgbToHexColor(0)).to.be.undefined;
+        expect(rgbToHexColor()).to.be.undefined;
+    });
+
+    it('returns undefined for param out of lower bound', () => {
+        expect(rgbToHexColor(-1, 0, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, -1, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 0, -1)).to.be.undefined;
+    });
+
+    it('returns undefined for param out of upper bound', () => {
+        expect(rgbToHexColor(256, 0, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 256, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 0, 256)).to.be.undefined;
+    });
+
+    it('returns undefined for floating number', () => {
+        expect(rgbToHexColor(1.1, 0, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 1.1, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 0, 1.1)).to.be.undefined;
+    });
+
+    it('returns undefined for string', () => {
+        expect(rgbToHexColor('1', 0, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, '1', 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 0, '1')).to.be.undefined;
+    });
+});
