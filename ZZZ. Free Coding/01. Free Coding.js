315d80372e1011acf11b68306a50e331e0cba078
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-06-22 16:27:56 +0300

    Test

--- ZZZ. Free Coding/01. Free Coding.js
+++ ZZZ. Free Coding/01. Free Coding.js
@@ -1,26 +1,32 @@
-function lockedProfile() {
-    Array.from(document.querySelectorAll('.profile button'))
-        .forEach(b => b.addEventListener('click', toggle));
+const { expect } =  require('chai');
+const { isSymmetric } = require('./test');
 
-    function toggle(event) {
-        const profile = event.target.parentElement;
-        const isActive = document.querySelector('input[type="radio"][value="unlock"]').checked;
+describe('Symmetry Checker', () => {
+    it('works with symmetric numeric array', () => {
+        expect(isSymmetric([1, 2, 2, 1])).to.be.true;
+    });
 
-        if (isActive) {
-            let div = profile.querySelector('div');
+    it('returns false for non-symmetric numeric array', () => {
+        expect(isSymmetric([1, 2, 3])).to.be.false;
+    });
 
-            if (event.target.textContent == 'Show more') {
-                event.target.textContent = 'Hide it';
-                div.style.display = 'block';
-            } else {
-                event.target.textContent = 'Show more';
-                div.style.display = '';
-            }
-        }
-    }
-}
+    it('returns false for non-array', () => {
+        expect(isSymmetric(5)).to.be.false;
+    });
 
-const obj = { 
-    name: Victor,
-    age: 18
-}
+    it('works with symmetric odd-length array', () => {
+        expect(isSymmetric([1, 2, 1])).to.be.true;
+    })  
+
+    it('works with symmetric string array', () => {
+        expect(isSymmetric(['a', 'b', 'b', 'a'])).to.be.true;
+    });
+
+    it('returns false for string param', () => {
+        expect(isSymmetric('abba')).to.be.false;
+    });
+
+    it('returns false for type mismatched elements', () => {
+        expect(isSymmetric([1, 2, '1'])).to.be.false;
+    });
+});
--- ZZZ. Free Coding/02. Free Coding.js
+++ ZZZ. Free Coding/02. Free Coding.js
@@ -1,3 +1,17 @@
-let arr = [1, 2, 3];
+function isSymmetric(arr) {
 
-console.log(isArray(arr));
+    if (!Array.isArray(arr)) {
+
+        return false; // Non-arrays are non-symmetric
+
+    }
+
+    let reversed = arr.slice(0).reverse(); // Clone and reverse
+
+    let equal = (JSON.stringify(arr) == JSON.stringify(reversed));
+
+    return equal;
+
+}
+
+module.exports = { isSymmetric };
