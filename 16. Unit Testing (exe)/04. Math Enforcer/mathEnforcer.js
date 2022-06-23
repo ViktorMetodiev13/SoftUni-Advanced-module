56a5aeaeed20445720be2c517ccabd03415ff1c6
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-06-23 16:43:21 +0300

    Changes on the Last Problems

--- 16. Unit Testing (exe)/04. Math Enforcer/mathEnforcer.js
+++ 16. Unit Testing (exe)/04. Math Enforcer/mathEnforcer.js
@@ -2,16 +2,20 @@ let mathEnforcer = {
     addFive: function (num) {
         if (typeof (num) !== 'number') {
             return undefined;
-        } return num + 5;
+        } 
+        return num + 5;
     },
     subtractTen: function (num) {
         if (typeof (num) !== 'number') {
             return undefined;
-        } return num - 10;
-    }, sum: function (num1, num2) {
+        } 
+        return num - 10;
+    }, 
+    sum: function (num1, num2) {
         if (typeof (num1) !== 'number' || typeof (num2) !== 'number') {
             return undefined;
-        } return num1 + num2;
+        } 
+        return num1 + num2;
     }
 };
 
--- 16. Unit Testing (exe)/04. Math Enforcer/mathEnforcer.test.js
+++ 16. Unit Testing (exe)/04. Math Enforcer/mathEnforcer.test.js
@@ -2,8 +2,36 @@ const { expect } = require("chai")
 const { mathEnforcer } = require("./mathEnforcer")
 
 describe('Tests for function mathEnforcer', () => {
-    it('do nothing', () => {
-
+    describe('Tests for inner funcion addFive', () => {
+        it('Should return correct result for num, negative num and floating', () => {
+            expect(mathEnforcer.addFive(5)).to.equal(10);
+            expect(mathEnforcer.addFive(1.1)).to.be.closeTo(6.1, 0.01);
+            expect(mathEnforcer.addFive(-5)).to.equal(0);
+        });
+        it('Incorrect input should return undefined', () => {
+            expect(mathEnforcer.addFive('5')).to.equal(undefined);
+        });
     });
 
+    describe('Tests for inner funcion subtractTen', () => {
+        it('Correct input should return num and floating num - 10', () => {
+            expect(mathEnforcer.subtractTen(10)).to.equal(0);
+            expect(mathEnforcer.subtractTen(10.1)).to.be.closeTo(0.1, 0.01);
+            expect(mathEnforcer.subtractTen(-10)).to.equal(-20);
+        });
+        it('Incorrect input should return undefined', () => {
+            expect(mathEnforcer.subtractTen('10')).to.equal(undefined);
+        });
+    });
+
+    describe('Tests for inner funcion sum', () => {
+        it('Incorrect input data should return undefined', () => {
+            expect(mathEnforcer.sum(5, '5')).to.equal(undefined);
+            expect(mathEnforcer.sum('5', 5)).to.equal(undefined);
+        });
+        it('Correct input data should return the sum of the two numbers', () => {
+            expect(mathEnforcer.sum(5.1, 5)).to.be.closeTo(10.1, 0.01);
+            expect(mathEnforcer.sum(-5, 5)).to.equal(0);
+        });
+    });
 });
