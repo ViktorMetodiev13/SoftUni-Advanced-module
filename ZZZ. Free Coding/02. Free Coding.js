579026834665dbf931684a0f0392de4c38cde964
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-06-27 23:28:21 +0300

    Changes

--- ZZZ. Free Coding/02. Free Coding.js
+++ ZZZ. Free Coding/02. Free Coding.js
@@ -1,17 +1,5 @@
-function isSymmetric(arr) {
-
-    if (!Array.isArray(arr)) {
-
-        return false; // Non-arrays are non-symmetric
-
-    }
-
-    let reversed = arr.slice(0).reverse(); // Clone and reverse
-
-    let equal = (JSON.stringify(arr) == JSON.stringify(reversed));
-
-    return equal;
-
+function solve(a, b) {
+    return a + b;
 }
 
-module.exports = { isSymmetric };
+console.log(solve(3,5))
